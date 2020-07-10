b51024f8c90bda58a800dece22cbde050dd5d35c
amneet <kauramneet986@gmail.com>
2020-07-10 21:39:09 +0530

    added new method in views

--- courses/views.py
+++ courses/views.py
@@ -3,3 +3,8 @@ from django.HttpResponse import HttpResponse
 
 def learn_django(request):
     return HttpResponse('hello django')
+    
+    
+def learn_python(request):
+    return HttpResponse('hello python')
+    
